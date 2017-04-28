efe467be3ce02eeacc9f50088d3aff8a251d67e3
Henok Teklu <henokzion@gmail.com>
2017-04-28 17:26:28 +0300

    configured to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -34,4 +35,4 @@ app.get('/about', (req, res)=>{
     })
 });
 
-app.listen(3000);
+app.listen(port);
